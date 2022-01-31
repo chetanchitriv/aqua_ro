9f000c7aacdc2b3dbff43610296633c29336b1fb
shank <shashanksahu1412@gmail.com>
2022-01-31 15:25:08 +0530

    notification crud api

--- backend/controller/notificationController.js
+++ backend/controller/notificationController.js
@@ -5,21 +5,31 @@ var todayDate = date.toISOString().slice(0, 10)
 //adding stock into the database
 exports.getAllNotification = async(req, res)=>{
     try {
-        const followupLead = await Lead.find({nextFollowupdate:`${todayDate}`})
+        // const followupLead = await Lead.find({nextFollowupdate:`${todayDate}`})
         // const followupLead = await Lead.find({nextFollowupdate:{ $eq: todayDate }})
-       
-        // const followupLead = await Lead.find({nextFollowupdate:{ $eq: "2022-01-18" }}) //for check menually
-        console.log(followupLead);
+        const followupLead = await Lead.find().where('nextFollowupdate').all("2022-01-28");
+        followupLead.find(function (err, followupLead) {
+            if (err) return handleError(err);
+            if (followupLead) {
+                res.status(201).json({lead:followupLead})
+                console.log("today's Lead has been sent");
+            }else{
+                res.json({Message:"Sorry No Lead found for today"})
+                console.log("No Leads for today")
+            }
+          });
+        // const followupLead = await Lead.find({nextFollowupdate:{ $in: "2022-01-18" }}) //for check menually
+        console.log(followupLead.nextFollowupdate);
         console.log(`${todayDate}`);
-        if(followupLead!=null){
-            res.status(201).json({lead:followupLead})
-            console.log("today's Lead has been sent");
+    //     if(followupLead){
+    //         res.status(201).json({lead:followupLead})
+    //         console.log("today's Lead has been sent");
             
-        // }else if(followupLead==[]){
-        }else{
-            res.json({Message:"Sorry No Lead found for today"})
-            console.log("No Leads for today")
-        }  
+    //     // }else if(followupLead==[]){
+    //     }else{
+    //         res.json({Message:"Sorry No Lead found for today"})
+    //         console.log("No Leads for today")
+    //     }  
     } catch (error) {
         res.status(400).json({error:error})
         console.log(error);
