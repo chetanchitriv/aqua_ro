4fb0b0c9f2cc44d9eaffa5533aeb50d64e4bdc9d
shank <shashanksahu1412@gmail.com>
2022-02-02 18:02:46 +0530

    label crud api

--- backend/routes/notificationRoute.js
+++ backend/routes/notificationRoute.js
@@ -4,10 +4,5 @@ const notificationController=require('../controller/notificationController')
 const verifyToken =require('../config/verifyToken')
 
 router.get('/', notificationController.getAllNotification,verifyToken.verifyToken);
-// router.post('/', notificationController.postNotification,verifyToken.verifyToken);
-// router.get('/:id', notificationController.getIdNotification,verifyToken.verifyToken);
-// router.put('/:id', notificationController.putStockTech,verifyToken.verifyToken);
-// router.delete('/:id', notificationController.deleteStockTech,verifyToken.verifyToken);
-
 
 module.exports=router
--- backend/routes/stockTechRouter.js
+++ backend/routes/stockTechRouter.js
@@ -5,13 +5,9 @@ const verifyToken =require('../config/verifyToken')
 stockTechController
 router.post('/', stockTechController.postStockTech,verifyToken.verifyToken);
 router.get('/', stockTechController.getallStockTech,verifyToken.verifyToken);
-// router.post('/', stockTechController.postStockTech);
 router.get('/:id', stockTechController.getStockTech,verifyToken.verifyToken);
-// router.get('/:id', stockTechController.getStockTech);
 router.put('/:id', stockTechController.putStockTech,verifyToken.verifyToken);
-// router.put('/:id', stockTechController.putStockTech);
 router.delete('/:id', stockTechController.deleteStockTech,verifyToken.verifyToken);
-// router.delete('/:id', stockTechController.deleteStockTech);
 
 
 module.exports=router
